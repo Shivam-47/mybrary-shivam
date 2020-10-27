0947de9e25d811ea99806cc65df6b5aa29a5c687
Shivam Handa <sh17@iitbbs.ac.in>
2020-10-27 17:41:45 +0530

    #4

--- routes/authors.js
+++ routes/authors.js
@@ -1,17 +1,30 @@
 const express = require('express')
 const router = express.Router()
-const Author = require('../models/author')
+//const Author = require('../models/author')
 //const Book = require('../models/book')
 
-//var bodyParser=require('body-parser');
 const bodyParser =  require('body-parser');
-const urlEncoded = bodyParser.urlencoded({extended:false});
+const Author = require('../models/author');
+const urlEncoded = bodyParser.urlencoded({limit:'10mb', extended:false});
 
 
 //All authors route//
-router.get('/',(req,res)=>{
-    res.render('authors/index');
-});
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name !== '') {
+      searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try {
+      const authors = await Author.find(searchOptions)
+      res.render('authors/index', {
+        authors: authors,
+        searchOptions: req.query
+      })
+    } catch {
+      res.redirect('/')
+    }
+  })
+  
 
 // New authors route//
 router.get('/new',function(req,res){
@@ -20,9 +33,22 @@ router.get('/new',function(req,res){
 
 
 //Create author route//
-router.post('/',urlEncoded,function(req,res){
+router.post('/',urlEncoded, async function(req,res){
     //res.send('New');
-    res.send(req.body.name); 
+    const author = new Author({
+        name: req.body.name
+    });
+
+    try {
+        const newAuthor =  await author.save()
+        //res.redirect(`authors/${newAuthor.id}`)
+        res.redirect(`authors`)
+    } catch {
+        res.render('authors/new',{
+        author: author,
+        errorMessage: 'Error creating author.'
+            })
+    }
 });
 
 module.exports =  router;
